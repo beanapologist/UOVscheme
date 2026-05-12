d533b14aa313d27d4d6fa950692450e1c913fa18
BeanApologist <sarmarin95@gmail.com>
2026-05-12 10:55:27 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.10.0"
 
 lean_lib UOVscheme where
-  globs := #[.subModules `UOVscheme]
-
+  -- No `globs` specified
+  
 lean_lib Test where
-  globs := #[.subModules `Test]
+  -- No `globs` specified
